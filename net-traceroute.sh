c30d65fe893050648858a8625c187ca46c151941
Philippe <philippe@lubrano.fr>
2017-10-13 16:17:32 +0200

    Menage

--- net-traceroute.sh
+++ net-traceroute.sh
@@ -5,12 +5,9 @@ date
 # traceroute
 sudo su -c "traceroute -I -A files.pharo.org"
 sudo su -c "traceroute -I -A ci.inria.fr"
-sudo su -c "traceroute -I -A www.inria.fr"
-sudo su -c "traceroute -I -A gforge.inria.fr"
 
 
 # wget
-
 wget  http://files.pharo.org/platform/Pharo6.0-linux.zip 
 wget https://ci.inria.fr/pharo/job/Pharo-6.0-Update-Step-5-Publish/lastSuccessfulBuild/artifact/Pharo6.1-64-linux.zip
 

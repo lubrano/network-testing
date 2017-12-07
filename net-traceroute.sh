c88f84c45da8449e980530181ef30218ade6d84e
lubrano <philippe@lubrano.fr>
2017-12-07 10:37:03 +0100

    Update net-traceroute.sh

--- net-traceroute.sh
+++ net-traceroute.sh
@@ -3,8 +3,8 @@
 date
 
 # traceroute
-sudo su -c "traceroute -I -A www-rocq.inria.fr"
-sudo su -c "traceroute -I -A files.pharo.org"
+#sudo su -c "traceroute -I -A www-rocq.inria.fr"
+sudo su -c "traceroute -I -A file-pharo.inria.fr"
 #sudo su -c "traceroute -I -A ci.inria.fr"
 #traceroute -A vodcast.upmc.fr
 
@@ -14,8 +14,8 @@ sudo su -c "traceroute -I -A files.pharo.org"
 #httping -h vodcast.upmc.fr -c 10
 
 # wget
-wget  https://www.rocq.inria.fr/gamma/gamma/Membres/CIPD/Loic.Marechal/Downloads/gm2.tgz
-wget  http://files.pharo.org/platform/Pharo6.0-linux.zip 
+#wget  https://www.rocq.inria.fr/gamma/gamma/Membres/CIPD/Loic.Marechal/Downloads/gm2.tgz
+wget  http://file-pharo.inria.fr/platform/Pharo6.0-linux.zip 
 #wget https://ci.inria.fr/pharo/job/Pharo-6.0-Update-Step-5-Publish/lastSuccessfulBuild/artifact/Pharo6.1-64-linux.zip
 #wget http://vodcast.upmc.fr/telecharger.php?file=/2017/colloquium_lip6-2017/01-colloquium_lip6-interfaces_homme_machine-michel_beaudoin_lafon-28_02_2017.mp4
 
